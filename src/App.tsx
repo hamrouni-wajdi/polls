c86a7f34cfc2b1d2bc7f25e3ed25a68f8bda9544
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2022-09-27 10:31:07 +0100

    imported navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
+    <Navbar />
            <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
