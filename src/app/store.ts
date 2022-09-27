313ca09d05c923064d2bc117c7e22dc0bae43078
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2022-09-27 16:38:33 +0100

    prepared search slice and search bar hooks

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-
+import searchReducer from '../components/searchBar/searchSlice';
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    search:searchReducer,
   },
 });
 
