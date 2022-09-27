fe612862ab522d92159800831310c6650da81e9e
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2022-09-27 12:00:36 +0100

    rendered footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import MyCarousel from './components/carousel/Carousel';
 import SecondCarousel from './components/carousel/SecondCarousel';
+import MyFooter from './components/footer/Footer';
 
 function App() {
   return (
@@ -15,52 +16,7 @@ function App() {
       Hello world!
     </h1>
     
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MyCarousel/>
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span className='underline text-5xl'>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <MyFooter/>  
     </div>
   );
 }
